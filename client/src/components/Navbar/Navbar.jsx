01bf84b00d877fbf33606a517a19b79f78a28914
Arpit <arpitagrawal2402@gmail.com>
2023-11-25 11:07:14 +0530

    navbar integrated

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,69 @@
+import { Link } from "react-router-dom";
+
+import logo from "../../assets/logo.png";
+import search from "../../assets/search-solid.svg";
+import bars from "../../assets/bars-solid.svg";
+import Avatar from "../../components/Avatar/Avatar";
+
+import "./Navbar.css";
+
+const Navbar = () => {
+  const User = null;
+
+  return (
+    <nav className="main-nav">
+      <div className="navbar">
+        <button className="slide-in-icon" onClick={() => { }}>
+          <img src={bars} alt="bars" width="15" />
+        </button>
+        <div className="navbar-1">
+          <Link to="/" className="nav-item nav-logo">
+            <img src={logo} alt="logo" />
+          </Link>
+          <Link to="/" className="nav-item nav-btn res-nav">
+            About
+          </Link>
+          <Link to="/" className="nav-item nav-btn res-nav">
+            Products
+          </Link>
+          <Link to="/" className="nav-item nav-btn res-nav">
+            For Teams
+          </Link>
+          <form>
+            <input type="text" placeholder="Search..." />
+            <img src={search} alt="search" width="18" className="search-icon" />
+          </form>
+        </div>
+        <div className="navbar-2">
+          {User === null ? (
+            <Link to="/Auth" className="nav-item nav-links">
+              Log in
+            </Link>
+          ) : (
+            <>
+              <Avatar
+                backgroundColor="#009dff"
+                px="10px"
+                py="7px"
+                borderRadius="50%"
+                color="white"
+              >
+                <Link
+                  to={`/Users/${User?.result?._id}`}
+                  style={{ color: "white", textDecoration: "none" }}
+                >
+                  {User.result.name.charAt(0).toUpperCase()}
+                </Link>
+              </Avatar>
+              <button className="nav-item nav-links" >
+                Log out
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+
+const mainRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <h1>home</h1>,
+      },
+      // {
+      //   path: "about",           
+      //   element: (
+      //     <Suspense fallback={<h1>Loading....</h1>}>
+      //       <About />
+      //     </Suspense>
+      //   ),
+      // },
+      {
+        path: "contact",
+        element: <h1 > contact</h1>,
+      },
+     
+      
+    ]
+  },
+])
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={mainRouter} />
   </React.StrictMode>,
 )
